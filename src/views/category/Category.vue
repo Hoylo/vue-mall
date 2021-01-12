ca42d17b50d8a7db5699695ac10b2a5e131a1b58
heping <1130231814@qq.com>
2021-01-12 18:10:33 +0800

    better-scroll

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,15 +1,43 @@
 <template>
-<div>
-  <h1>分类</h1>
+<div class="wrapper" ref="wrapper">
+
 </div>
 </template>
 
 <script>
+import BScroll from "better-scroll"
+
 export default {
-  name: "Category"
+  name: "Category",
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: 3,
+        click: true,
+        pullUpLoad: true
+      })
+
+      this.scroll.on('scroll', (position) => {
+        console.log(position)
+      })
+
+      this.scroll.on('pullingUp', () => {
+
+      })
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.wrapper {
+  height: 200px;
+  background: #ff5777;
+  overflow: hidden;
+}
 </style>
