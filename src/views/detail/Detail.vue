cfcdf7d85c22a7382c60ccb7637982be85f85438
heping <1725683615@qq.com>
2021-01-14 23:45:51 +0800

    详情页顶部

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,14 @@
 <template>
-<div>{{ iid }}</div>
+<div class="detail">
+  <detail-nav-bar/>
+</div>
 </template>
 
 <script>
+import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
+
+import {getDetail} from "@/network/detail";
+
 export default {
   name: "Detail",
   data() {
@@ -10,8 +16,19 @@ export default {
       iid: null
     }
   },
+  methods: {
+  },
+  components: {
+    DetailNavBar
+  },
   created() {
     this.iid = this.$route.params.iid
+
+    getDetail(this.iid).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   }
 }
 </script>
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav-bar class="nav-bar">
+      <div slot="left" class="left" @click="back">
+        <img src="@/assets/images/common/back.svg" alt="">
+      </div>
+      <div slot="center" class="center">
+        <div v-for="(item, index) in navItems" :key="index" class="nav-item"
+             @click="clickNavItem(index)" :class="{active: currentIndex === index}">{{item}}
+        </div>
+      </div>
+    </nav-bar>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar"
+
+export default {
+  name: "DetailNavBar",
+  data() {
+    return {
+      currentIndex: 0,
+      navItems: ['商品', '参数', '评论', '推荐'],
+    }
+  },
+  components: {
+    NavBar
+  },
+  methods: {
+    clickNavItem(index) {
+      this.currentIndex = index
+    },
+    back() {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.left img {
+  padding-left: 1px;
+  padding-top: 10px;
+}
+.center {
+  display: flex;
+}
+.nav-item {
+  flex: 1;
+}
+.center .active {
+  color: var(--color-high-text);
+}
+</style>
