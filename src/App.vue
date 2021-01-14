326dea7ca317971c80cc0f2e12fe37eb096320d9
heping <1725683615@qq.com>
2021-01-15 00:05:12 +0800

    详情轮播图

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" v-cloak>
-    <keep-alive>
+    <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
     <main-tab-bar/>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,30 +1,36 @@
 <template>
 <div class="detail">
   <detail-nav-bar/>
+  <detail-swiper :top-images="topImages"/>
 </div>
+
 </template>
 
 <script>
-import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
+import DetailNavBar from "@/views/detail/childComps/DetailNavBar"
+import DetailSwiper from "@/views/detail/childComps/DetailSwiper";
 
-import {getDetail} from "@/network/detail";
+import {getDetail} from "@/network/detail"
 
 export default {
   name: "Detail",
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   methods: {
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
   created() {
     this.iid = this.$route.params.iid
 
     getDetail(this.iid).then(res => {
+      this.topImages = res.result.itemInfo.topImages
       console.log(res);
     }).catch(err => {
       console.log(err);
